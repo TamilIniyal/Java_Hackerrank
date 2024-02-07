e655db234e0340be02f189dc5ce0131196a3905f
Tamil Iniyal T <105866566+TamilIniyal@users.noreply.github.com>
2024-02-07 09:46:43 +0530

    Add files via upload
    Given two strings of lowercase English letters, A and B , perform the following operations:
    
    Sum the lengths of  A and B.
    Determine if A is lexicographically larger than B (i.e.: does A come before B  in the dictionary?).
    Capitalize the first letter in A and B and print them on a single line, separated by a space.
    
    Sample Input:
    hello
    java
    
    Sample Output:
    9
    No
    Hello Java

---
+++ Solution.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        int A_len = A.length();
+        int B_len = B.length();
+        int sum = A_len + B_len;
+        System.out.println(sum);
+        
+        int retval = A.compareTo(B);
+        if (retval>0){
+           System.out.println("Yes"); 
+        }
+        else{
+            System.out.println("No");
+        }
+        
+        String s1 = A.substring(0,1).toUpperCase() + A.substring(1,A_len); 
+        String s2 = B.substring(0,1).toUpperCase() + B.substring(1,B_len);
+        System.out.println(s1 + " " + s2);
+        
+    }
+}
+
+
+
